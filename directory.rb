8fd8422ddfba3624cde7db1f075dc10c91e776d6
jpates1 <jamesandrewpates@gmail.com>
2023-02-01 12:03:22 +0000

    add the interactive menu to directory.rb

--- directory.rb
+++ directory.rb
@@ -1,16 +1,40 @@
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffery Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+def interactive_menu
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+
+    case selection
+      when "1"
+        students = input_students
+      when "2"
+        print_header
+        print(students)
+        print_footer(students)
+      when "9"
+        exit
+      else
+        puts "I don't know what you meant, try again"
+    end
+  end
+end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish just hit return twice"
+
+  students = []
+
+  name = gets.chomp
+  
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
 
 def print_header
   puts "The Students of Villains Academy"
@@ -27,6 +51,4 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
-print_header
-print(students)
-print_footer(students)
+interactive_menu
