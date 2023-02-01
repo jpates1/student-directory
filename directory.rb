738267ebd49125f7cbf3e4899f9b62fbd81a21c0
jpates1 <jamesandrewpates@gmail.com>
2023-02-01 11:21:54 +0000

    update directory.rb (refactor with methods)

--- directory.rb
+++ directory.rb
@@ -11,11 +11,21 @@ students = [
 "Joffery Baratheon",
 "Norman Bates"
  ]
-
-puts "The Students of Villains Academy"
-puts "-------------"
-students.each do |student|
-  puts student
+def print_header
+  puts "The Students of Villains Academy"
+  puts "-------------"
 end
 
-puts "Overall, we have #{students.count} great students"
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
